4980c17d8549cf72915f691ff12479f7284fb591
Nandanandan Mishra <nm00436182@techmahindra.com>
2020-04-26 18:44:12 +0100

    addint input data

--- login-device.py
+++ login-device.py
@@ -1,11 +1,20 @@
 import telnetlib
 from login_data import creds
+import pandas as pd
 
 # import credentials
+
+
 
 host_ip = creds["host_ip"]
 username = creds["username"]
 password = creds["password"]
+
+# read data from excelfile
+file_name = device_input_data.xlsx
+read_file = pd.read_excel(file_name, sheet_name='Sheet1')
+for headers in read_file.columns:
+    print(headers)
 
 # commands to be executed on device
 commands = {
@@ -18,7 +27,7 @@ print(f"Telnet to device: {host_ip}")
 """
 code section  to login and to device and get the output
 """
- 
+
 tn = telnetlib.Telnet(host_ip)
 tn.read_until(b"Username: ")
 tn.write(username.encode('ascii') + b"\n")
