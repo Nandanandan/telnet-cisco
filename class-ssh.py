eb6f0c0f9d7a8463d2fc04afbc809a6ada88a0ba
Nandanandan Mishra <nm00436182@techmahindra.com>
2020-05-05 13:05:04 +0100

    updated data for ssh

--- class-ssh.py
+++ class-ssh.py
@@ -1,4 +1,4 @@
-import paramiko
+from netmiko import ConnectHandler
 import getpass
 
 class ssh:
@@ -6,18 +6,25 @@ class ssh:
     def __init__(self):
         pass
 
-    def ssh_device(self, host_ip, username, password):
-        session = paramiko.SSHClient()
-        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client = session.connect(hostname=host_ip, username=username, password=password)
-        stdin, stdout, stderr = client.exec_command('terminal length 0', 'show run')
-        client.close()
-        return stdout
+    def ssh_device(self, **device):
+        net_connect = ConnectHandler(**device)
+        output = net_connect.send_command('show ip int brief')
+        return output
+
 
 
 if __name__ == "__main__":
+
     ip = input(r"IP:")
     usern = input(r"user:")
     passw = getpass.getpass("Pass:")
-    data = ssh().ssh_device(ip,usern,passw)
+
+    device = {
+        'device_type': 'cisco_ios',
+        'host': ip,
+        'username': usern,
+        'password': passw,
+    }
+    obj = ssh()
+    data = obj.ssh_device(**device)
     print(data)
